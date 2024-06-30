1ac0f4d7835069974cb7364a190c9868976eb81f
Ezinwa Victor <42660335+somtorizm@users.noreply.github.com>
2024-06-30 23:30:47 +0100

    implement loading section (#2)

--- app/src/main/java/com/vectorincng/socketchat/ui/theme/screens/LoginScreen.kt
+++ app/src/main/java/com/vectorincng/socketchat/ui/theme/screens/LoginScreen.kt
@@ -5,48 +5,41 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandIn
 import androidx.compose.animation.slideInHorizontally
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.vectorincng.socketchat.R
 import com.vectorincng.socketchat.extensions.EmailTextField
 import com.vectorincng.socketchat.extensions.LoginButton
-import com.vectorincng.socketchat.extensions.PasswordTextField
 import com.vectorincng.socketchat.extensions.VerticalSpacer
-import com.vectorincng.socketchat.extensions.signUpText
 
 @ExperimentalAnimationApi
 @Composable
-fun LoginScreen() {
+fun LoginScreen(navController: NavHostController) {
     var visibility by remember { mutableStateOf(false) }
     val density = LocalDensity.current
     val config = LocalConfiguration.current
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(color = Color.White)
                 .statusBarsPadding()
         ) {
+            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+
             AnimatedVisibility(
                 visible = visibility,
                 enter = slideInHorizontally(
@@ -57,17 +50,13 @@ fun LoginScreen() {
                 )
 
             ) {
-                LoginAppBar(
-                    modifier = Modifier
-                        .height(56.dp)
-                        .fillMaxWidth()
-                )
             }
 
             LoginForm(
                 visibility,
                 density,
-                config.screenWidthDp
+                config.screenWidthDp,
+                navController
             )
         }
         LaunchedEffect(key1 = "key1", block = { visibility = true })
@@ -79,16 +68,25 @@ fun LoginAppBar(modifier: Modifier) {
         modifier
     ) {
         Text(
-            "Log in",
+            "Socket Chat Login",
             modifier = Modifier.align(Alignment.Center),
-            fontSize = 18.sp
+            style = TextStyle(
+                color = Color(0xFFCCCCCC),
+                fontSize = 16.sp,
+                fontFamily = FontFamily(Font(R.font.sen))
+            )
         )
     }
 }
 
 @ExperimentalAnimationApi
 @Composable
-fun LoginForm(visibility: Boolean, density: Density, screenWidth: Int) {
+fun LoginForm(
+    visibility: Boolean,
+    density: Density,
+    screenWidth: Int,
+    navController: NavHostController
+) {
 
     val slideInHorizontallyFromLeftAnimation = slideInHorizontally(
         initialOffsetX = {
@@ -107,45 +105,31 @@ fun LoginForm(visibility: Boolean, density: Density, screenWidth: Int) {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(horizontal = 24.dp)
-            .absolutePadding(top = 16.dp)
+            .padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.Center
     ) {
+
+        LoginAppBar(
+            modifier = Modifier
+                .height(56.dp)
+                .fillMaxWidth()
+        )
+
+        VerticalSpacer(value = 16)
 
         AnimatedVisibility(
             visible = visibility,
             enter = slideInHorizontallyFromLeftAnimation,
             content = { EmailTextField() }
         )
-        VerticalSpacer(value = 16)
-
-        AnimatedVisibility(
-            visible = visibility,
-            enter = slideInHorizontallyFromRightAnimation,
-            content = { PasswordTextField() }
-        )
-
 
         VerticalSpacer(value = 16)
-
-        AnimatedVisibility(
-            visible = visibility,
-            enter = expandInFromCenterAnimation,
-            content = {
-                Text(
-                    "Forgot Password?",
-                    fontSize = 15.sp,
-                    color = Color.Blue,
-                    fontWeight = FontWeight.SemiBold
-                )
-            }
-        )
-        VerticalSpacer(value = 32)
 
         AnimatedVisibility(
             visible = visibility,
             enter = expandInFromCenterAnimation
         ) {
-            LoginButton(onClick = { }, color = Color.Blue) {
+            LoginButton(onClick = { navController.navigate("home")}, color = Color.Blue) {
                 Text(
                     "Log in", color = Color.White,
                     fontWeight = FontWeight.Bold
@@ -155,21 +139,5 @@ fun LoginForm(visibility: Boolean, density: Density, screenWidth: Int) {
 
 
         VerticalSpacer(value = 24)
-        AnimatedVisibility(
-            visible = visibility,
-            enter = expandInFromCenterAnimation
-        ) {
-            ClickableText(
-                text = signUpText(),
-                style = TextStyle(
-                    textAlign = TextAlign.Center,
-                    fontSize = 17.sp,
-                    letterSpacing = 1.5.sp
-                ),
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                // perform action when text clicked
-            }
         }
-    }
 }
